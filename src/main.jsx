a34c1e0b2863418077430cbfe3674aed772d352f
Utkarsh Jha <utkarsh28jha@outlook.com>
2026-02-24 15:01:14 -0500

    Add App route and keep RAG page

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
 import Rag from "./pages/Rag.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Navigate to="/rag" replace />} />
+      <Route path="/" element={<App />} />
       <Route path="/rag" element={<Rag />} />
     </Routes>
   </BrowserRouter>
